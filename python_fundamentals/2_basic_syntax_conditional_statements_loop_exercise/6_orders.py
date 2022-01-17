027610e145e29fd3cadf6fcd65f0273f7edef4e2
Administrator <astrikol@gmail.com>
2022-01-17 14:58:23 +0200

    a

--- python_fundamentals/2_basic_syntax_conditional_statements_loop_exercise/6_orders.py
+++ python_fundamentals/2_basic_syntax_conditional_statements_loop_exercise/6_orders.py
@@ -1,2 +1,11 @@
-orders = int(input)
-cofee is gone
+orders = int(input())
+total = 0
+
+for i in range(orders):
+    capsule_price = float(input())
+    days = int(input())
+    capsule_count = int(input())
+    price = (capsule_price * capsule_count) * days
+    total += price
+    print(f"The price for the coffee is: ${price:.2f}")
+print(f"Total: ${total:.2f}")
