3683cc9a48dbb98b3f6355b79155500be2d8b5b4
astri <astrikol@gmail.com>
2022-01-19 18:06:01 +0200

    a

--- python_fundamentals/3_basic_syntax_conditional_statements_loop_more_exercise/5_how_much_coffee_do_you_need.py
+++ python_fundamentals/3_basic_syntax_conditional_statements_loop_more_exercise/5_how_much_coffee_do_you_need.py
@@ -5,24 +5,12 @@ while not end:
     if coffee > 5:
         print('You need extra sleep')
         break
-    if event == "END":
+    elif event == "END":
         end = True
         break
-    elif event == "dog":
+    elif event == "dog" or event == "cat" or event == "coding" or event == "movie":
         coffee += 1
-    elif event == "DOG":
+    elif event == "DOG" or event == "CAT" or event == "CODING" or event == "MOVIE":
         coffee += 2
-    elif event == "cat":
-        coffee += 1
-    elif event == "CAT":
-        coffee += 2
-    elif event == "coding":
-        coffee += 1
-    elif event == "CODING":
-        coffee += 2
-    elif event == "movie":
-        coffee += 1
-    elif event == "MOVIE":
-        coffee += 2
-if coffee < 5:
+if coffee <= 5:
     print(coffee)
