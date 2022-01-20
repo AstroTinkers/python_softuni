065c3744ca96ebfdcce1f1a5e7065ef31c6e617c
astri <astrikol@gmail.com>
2022-01-20 17:55:30 +0200

    a

--- set_check.py
+++ set_check.py
@@ -2,13 +2,16 @@ proverka = set()
 while True:
     vhod = input("Input(Press q to exit):")
     if vhod == "q":
-        final = list(proverka)
-        final.sort()
-        textfile = open("Results.txt", "w")
-        for elements in final:
-            textfile.write(elements + "\n")
-        textfile.close()
-        print(final)
+        if len(proverka) == 0:
+            break
+        else:
+            final = list(proverka)
+            final.sort()
+            textfile = open("Results.txt", "w")
+            for elements in final:
+                textfile.write(elements + "\n")
+            textfile.close()
+            print(final)
         break
     elif vhod in proverka:
         print("You already got one.")
