1bc1da2919c3a009647a4cc721ce83c5ff30a083
Astrikol <astrikol@gmail.com>
2022-03-18 18:24:13 +0200

    a

--- python_fundamentals/25_regex_lab/2_match_phone_number.py
+++ python_fundamentals/25_regex_lab/2_match_phone_number.py
@@ -1,6 +1,6 @@
 import re
 phones = input()
-matches = re.findall(r"(\+359(\s{1}|-)2\2\d{3}\2\d{4})\b", phones)
+matches = re.findall(r"(\+359(\s|-)2\2\d{3}\2\d{4})\b", phones)
 valid_phones = []
 for i in matches:
     valid_phones.append(i[0])
