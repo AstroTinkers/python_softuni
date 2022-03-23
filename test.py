3fedb13622e0e59f6d68e04897a0ac4669abf906
Administrator <astrikol@gmail.com>
2022-03-23 15:30:38 +0200

    Solved some more tasks on 23.03.2022

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ furniture_bought = {}
 pattern = r">>(?P<furniture>\w+)<<(?P<price>\d*\.?\d+?)!(?P<quantity>\d+)"
 while not command == "Purchase":
     valid_purchase = re.match(pattern, command)
-    if valid_purchase and valid_purchase.group('furniture' and 'price' and 'quantity'):
+    if valid_purchase.group('furniture' and 'price' and 'quantity'):
         sum_of_purchase = float(valid_purchase.group('price')) * int(valid_purchase.group('quantity'))
         if sum_of_purchase > 0:
             furniture_bought.setdefault(valid_purchase.group('furniture'), 0)
