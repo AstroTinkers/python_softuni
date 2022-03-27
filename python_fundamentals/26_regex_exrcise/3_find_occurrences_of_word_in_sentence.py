d87c8067f1e2d1e6cf444cdf63f9e9ed76f1af7b
astri <astrikol@gmail.com>
2022-03-27 15:47:29 +0300

    a

--- python_fundamentals/26_regex_exrcise/3_find_occurrences_of_word_in_sentence.py
+++ python_fundamentals/26_regex_exrcise/3_find_occurrences_of_word_in_sentence.py
@@ -1,6 +1,7 @@
 import re
+word_filter = r"\b[\S]+\b"
 input_text = input()
+matched_input = re.findall(word_filter, input_text)
 search_word = input().lower()
-pattern = re.compile(search_word)
-matches = re.findall(pattern, input_text.lower())
-print(len(matches))
+found = [word for word in matched_input if word.lower() == search_word]
+print(len(found))
