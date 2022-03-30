6f8c5e4e8e1fa2616fd9cf0d89d9ff342270b36c
astri <astrikol@gmail.com>
2022-03-31 00:32:17 +0300

    Solved 5.Dragon Army from Dictionaries: More Exercises

--- python_fundamentals/21_dictionaries_more_exercise/5_dragon_army.py
+++ python_fundamentals/21_dictionaries_more_exercise/5_dragon_army.py
@@ -1,50 +1,42 @@
-def add_dragon(type, stat, data):
-    d_type, d_stat, d_damage, d_health, d_armor = data.split()
-    if d_damage == 'null':
-        d_damage = 45
-    else:
-        d_damage = int(d_damage)
-    if d_health == 'null':
-        d_health = 250
-    else:
-        d_health = int(d_health)
-    if d_health == 'null':
-        d_health = 250
-    else:
-        d_health = int(d_health)
-    if d_armor == 'null':
-        d_armor = 10
-    else:
-        d_armor = int(d_armor)
-    type[d_stat] = d_type
-    stat[d_stat] = [d_damage, d_health, d_armor]
+class Dragon:
+    def __init__(self, dragon_line):
+        self.type, self.name, self.dmg, self.health, self.armor = dragon_line.split()
+        self.dmg = 45 if self.dmg == 'null' else int(self.dmg)
+        self.health = 250 if self.health == 'null' else int(self.health)
+        self.armor = 10 if self.armor == 'null' else int(self.armor)
+
+    def __repr__(self):
+        return f"-{self.name} -> damage: {self.dmg}, health: {self.health}, armor: {self.armor}"
+
+    def __eq__(self, other):
+        return self.type == other.type and self.name == other.name
 
 
-def average_stats(type, stats):
-    damage = 0
-    health = 0
-    armor = 0
-    for key1, value1 in type.items():
-        for key in stats[value1]:
-            damage += value[0]
-            health += value[1]
-            armor += value[2]
-        break
-    damage = damage / len(stats)
-    health = health / len(stats)
-    armor = armor / len(stats)
-    return f"{damage:.2f}, {armor:.2f}, {health:.2f}"
-
-
-dragons = {}
-stats = {}
-dragons_count = int(input())
-for dragon in range(dragons_count):
-    dragons_input = input()
-    add_dragon(dragons, stats, dragons_input)
-for dragon_type in dragons.keys():
-    print(dragons[dragon_type])
-    # avg_dmg, avg_health, avg_armor = average_stats(dragons, stats).split(", ")
-    # print(f"{dragon_type}::({avg_dmg}/{avg_health}/{avg_armor})")
-    # for stats[dragon_type]:
-    print(f"-{dragon_type} -> damage: {stats[dragon_type][0]}, health: {stats[dragon_type][1]}, armor: {stats[dragon_type][2]}")
+given_dragons = int(input())
+dragons = []
+for info in range(given_dragons):
+    dragon_info = input()
+    dragon_temp = Dragon(dragon_info)
+    if dragon_temp in dragons:
+        dragon_index = dragons.index(dragon_temp)
+        dragons[dragon_index] = dragon_temp
+    else:
+        dragons.append(dragon_temp)
+dragon_types = {}
+for dragon in dragons:
+    dragon_types.setdefault(dragon.type, [])
+    dragon_types[dragon.type].append(dragon)
+dragon_types = {d_type: sorted(drs, key=lambda x: x.name) for d_type, drs in dragon_types.items()}
+for drg_type in dragon_types:
+    avg_health, avg_armor, avg_dmg = 0, 0, 0
+    len_dragon = len(dragon_types[drg_type])
+    for d in dragon_types[drg_type]:
+        avg_health += d.health
+        avg_armor += d.armor
+        avg_dmg += d.dmg
+    avg_health /= len_dragon
+    avg_armor /= len_dragon
+    avg_dmg /= len_dragon
+    print(f"{drg_type}::({avg_dmg:.2f}/{avg_health:.2f}/{avg_armor:.2f})")
+    for value in dragon_types[drg_type]:
+        print(value)
