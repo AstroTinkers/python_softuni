6d893e1e561b3f94f91063eae7cf34c5a5f716a2
Administrator <astrikol@gmail.com>
2022-04-11 11:51:03 +0300

    Worked a little on an assignment.

--- Assignement/classes.py
+++
@@ -1,5 +0,0 @@
-class Car:
-    def __init__(self, data):
-        self.model, self.plate, self.color, self.vin, self.part_price = data.split(", ")
-        self.vin = int(self.vin)
-        self.part_price = float(self.part_price) * 1.2
